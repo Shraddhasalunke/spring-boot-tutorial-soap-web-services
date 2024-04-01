7901049d4af3888a602128afcba913638e36aeac
Shraddha Salunke <91833741+Shraddhasalunke@users.noreply.github.com>
2024-04-01 17:08:05 +0530

    Add files via upload

---
+++ src/test/java/com/in28minutes/springboot/soap/web/services/example/SpringBootTutorialSoapWebServicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.in28minutes.springboot.soap.web.services.example;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class SpringBootTutorialSoapWebServicesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
